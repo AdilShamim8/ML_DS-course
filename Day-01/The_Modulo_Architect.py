aa4e08eba8d6cd5c597da02ab17e840130d44f37
Adil Shamim <157037615+AdilShamim8@users.noreply.github.com>
2025-12-14 11:43:39 +0600

    Add files via upload

---
+++ Day-01/The_Modulo_Architect.py
@@ -0,0 +1,9 @@
+X = (int(input("Enter total seconds: ")))
+    
+hours = X // 3600          
+remaining = X % 3600      
+minutes = remaining // 60              
+seconds = remaining % 60               
+    
+print(f"{X} seconds = {hours}h {minutes}m {seconds}s")
+print(remaining)
