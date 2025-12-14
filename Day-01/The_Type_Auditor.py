8c0446f3d65638dfdf60cf02a35f2e53461121d6
Adil Shamim <157037615+AdilShamim8@users.noreply.github.com>
2025-12-14 11:44:28 +0600

    Add files via upload

---
+++ Day-01/The_Type_Auditor.py
@@ -0,0 +1,10 @@
+X = input("Enter a number: ")
+
+print(f"input type: {type(X)}")
+
+converted = float(X)
+
+print(f"Converted type:  {type(converted)}") 
+
+print(f"input id: {id(X)}")
+print(f"Converted id: {id(converted)}") 
