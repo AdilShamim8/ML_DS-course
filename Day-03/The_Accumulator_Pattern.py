bade29b8ac1a524bc529cb9da5d2f31eb65b84f2
Adil Shamim <157037615+AdilShamim8@users.noreply.github.com>
2025-12-26 10:33:26 +0600

    Delete Day-03 directory

--- Day-03/The_Accumulator_Pattern.py
+++
@@ -1,5 +0,0 @@
-N = int(input("Enter a number N: "))
-total = 0
-for i in range(1, N + 1):
-    total += i
-print(f"The sum of numbers from 1 to {N} is: {total}")
--- Day-03/The_Infinite_Guardian.py
+++
@@ -1,4 +0,0 @@
-while True:
-    password = input("Enter password (or 'stop' to exit): ")
-    if password.lower() == 'stop':
-        break
